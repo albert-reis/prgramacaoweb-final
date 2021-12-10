ec9c03a54af187c13fa188ede9fa08f333f4abdb
Marcelo Albert <marcelo.albert.reis@hotmail.com>
2021-12-09 21:33:07 -0300

    Adicionado saída do CRUD de Obras no blade do home

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Obra;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index () {
-        return view('dashboard.content-home');
+        $obras = Obra::all();
+        return view('dashboard.content-home', compact('obras'));
     }
 }
